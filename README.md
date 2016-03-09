9707d9ea7e677304844fa69e30eba186901d90f1
Court Ewing <court@epixa.com>
2016-03-09 08:16:24 -0500

    Project files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Elasticsearch Scrolling Search
+
+Scroll through all Elasticsearch search results as a single stream
---
+++ test/lib/tmp.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('tmp', () => {
+  it('is true', () => {
+    expect(true).to.equal(true);
+  });
+});
